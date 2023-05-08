0de6d030b895fc8da9eaf082dded9f2c79f01cf1
Matt <mdeckerr@gmail.com>
2023-05-08 07:49:24 -0500

    Run

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const fetch = require("node-fetch");
 //Assign Port from env OR assign to port 3000
 const port = process.env.PORT || 3000;
 
-//Makes public folders public
 app.use(express.static("public"));
-
-//API Variabes
-// let endpoint = "https://api.coindesk.com/v1/bpi/currentprice.json"
+app.use(express.json());
 
 //ROUTING
 //Redirect from base url to /home url
+
 app.get("/", (req, res) => res.redirect("/home"));
 
-//Connect home.ejs to website
+app.listen(port, () => console.log(`Bitcoing Project on ${port}`));
+
+// // Connect home.ejs to website
 app.get("/home", (req, res) => {
   console.log(req.query);
   let endpoint = "https://api.coindesk.com/v1/bpi/currentprice.json";
@@ -32,31 +32,37 @@ app.get("/home", (req, res) => {
     })
     .then((parsedData) => {
       console.log(parsedData);
-      const { code, symbol, rate } = parsedData.bpi[req.query.bitcoinPrice];
-      console.log(req);
-      res.render("index.ejs", { data: code, symbol, rate });
+      let code = parsedData.bpi.USD.code;
+      let symbol = parsedData.bpi.USD.symbol;
+      let rate = parsedData.bpi.USD.rate_float;
+      let newRate = parsedData.bpi.USD.rate_float.toFixed(2);
+      console.log(rate);
+      res.render("index.ejs", { code, symbol, newRate });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-// app.get("/home", (req, res) => {
-//   console.log(req.parsedData);
-//   res.render("home.ejs", { data: parsedData.USD });
-// });
-
-//Catchall redirect
-// app.get("/*", (req, res) => res.redirect("/home"));
-
-app.listen(port, () => console.log(`Bitcoing Project on ${port}`));
-
-app.use(express.static("public"));
-
-// <!-- <h3>The Current Price of Bitcoin in <%= data.USD.code %>:</h3>
-// <h2><%- data.USD.symbol%><%= ` ${data.USD.rate}` %></h2>
-// <% if (bitcoinPrice === USD) { %>
-//   <p><%= `The Current Price of Bitcoin in ${data.USD.code}}`</p>
-//   <p><$- data.USD.symbol %><%= ` ${data.USD.rate}` %></p>
-// <% } else { %>
-//   <p><%= data.GBP.symbol %></p> -->
+app.get("/bitcoinPrice", (req, res) => {
+  let endpoint = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  let { bitcoinPrice } = req.query;
+  console.log(bitcoinPrice);
+  fetch(endpoint)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error("API Data Error");
+      }
+    })
+    .then((parsedData) => {
+      const { code, symbol, rate, rate_float } =
+        parsedData.bpi[req.query.bitcoinPrice];
+      let newRate = rate_float.toFixed(2);
+      res.render("index.ejs", { code, symbol, newRate });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
